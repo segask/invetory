52d5bd808e8c1962ed9664e15d4dd0ead2ba5e52
segask <119155621+segask@users.noreply.github.com>
2026-05-01 23:09:31 +0000

    ver.0.3

--- client/client.lua
+++ client/client.lua
@@ -8,14 +8,24 @@ Citizen.CreateThread(function()
         TriggerEvent('esx:getSharedObject', function(obj) ESX = obj end)
         Citizen.Wait(100)
     end
+    print("^2[Inventory] ESX успешно загружен^7")
 end)
 
 -- Регистрация клавиши для открытия инвентаря (по умолчанию I, игрок может изменить)
-RegisterKeyMapping('inventory', 'Открыть инвентарь', 'keyboard', 'I')
+RegisterKeyMapping('openInventory', 'Открыть инвентарь', 'keyboard', 'I')
 
 -- Открытие инвентаря
-RegisterCommand('inventory', function()
-    if not inventoryOpen and ESX ~= nil then
+RegisterCommand('openInventory', function()
+    if ESX == nil then
+        TriggerEvent('chat:addMessage', {
+            color = {255, 0, 0},
+            multiline = true,
+            args = {"Система", "Ошибка при загрузке инвентаря"}
+        })
+        return
+    end
+    
+    if not inventoryOpen then
         ESX.TriggerServerCallback('inventory:loadInventory', function(inventory)
             playerInventory = inventory
             SendNUIMessage({
@@ -38,7 +48,6 @@ end)
 -- Использование предмета
 RegisterNUICallback('useItem', function(data, cb)
     local item = data.item
-    -- Логика использования предмета
     TriggerServerEvent('inventory:useItem', item)
     cb('ok')
 end)
@@ -47,7 +56,6 @@ end)
 RegisterNUICallback('moveItem', function(data, cb)
     local fromSlot = data.fromSlot
     local toSlot = data.toSlot
-    -- Логика перемещения
     playerInventory[fromSlot], playerInventory[toSlot] = playerInventory[toSlot], playerInventory[fromSlot]
     TriggerServerEvent('inventory:saveInventory', playerInventory)
     cb('ok')
